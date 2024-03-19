b42fe3609966d3d10401f31d83400aa6bfe89d1e
tranvancanhtozan <85920372+tranvancanhtozan@users.noreply.github.com>
2024-03-19 17:58:43 +0900

    酒倉デポAGF：1回目 ・ MSSQLAccessとtable_switch要るから戻す。 ・ プログラム名はハハイフンがダメ-＞アンダーに変更 ・ コントロールを追加

---
+++ Controllers/v1/AgfKanbanRead.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace sumaken_api_agf.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class AgfKanbanRead : ControllerBase
+    {
+        // GET: api/<AgfKanbanRead>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AgfKanbanRead>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AgfKanbanRead>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AgfKanbanRead>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AgfKanbanRead>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Controllers/v1/AgfLanenoRead.cs
+++ Controllers/v1/AgfLanenoRead.cs
@@ -2,40 +2,40 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace SakaguraAGFWebApi.Controllers
+namespace sumaken_api_agf.Controllers.v1
 {
     [ApiVersion("1.0")]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
-    public class ShipmentAgfController : ControllerBase
+    public class AgfLanenoRead : ControllerBase
     {
-        // GET: api/<AGFShipmentController>
+        // GET: api/<AgfLanenoRead>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<AGFShipmentController>/5
+        // GET api/<AgfLanenoRead>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<AGFShipmentController>
+        // POST api/<AgfLanenoRead>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<AGFShipmentController>/5
+        // PUT api/<AgfLanenoRead>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<AGFShipmentController>/5
+        // DELETE api/<AgfLanenoRead>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
